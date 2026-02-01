83e335593dd4cbc1c5bd86afb82e682e1b76b7df
MCa818b <max78c.minecraft@gmail.com>
2026-02-01 16:07:36 +0800

    Add files via upload

---
+++ v1.21.11/net_client_minecraft-Xmx1024M-Xms1024M_jar-jre8_json-jdk21.bat
@@ -0,0 +1,4 @@
+@echo off
+title Minecraft
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.21.11.jar nogui
+pause
---
+++ v1.21.11/net_client_minecraft-paper.bat
@@ -0,0 +1,3 @@
+@echo off
+title paper
+java -Xmx1024M -Xms1024M -jar paper-1.21.11-99.jar nogui
